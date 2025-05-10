fdbbb27c36a9e2174922d89eb1abf7578c33c4d5
SamRB-dev <sadimrahmanbadhon@gmail.com>
2025-05-10 13:35:18 +0200

    Capture camera feed with cv2 and feed the frame to custom yolo model

--- CaptureSmiles.py
+++ CaptureSmiles.py
@@ -1,15 +1,36 @@
 # -*- coding: utf-8 -*-
 from ultralytics import YOLO
 from rich.console import Console
+import cv2 
 
 # Rich Console for colored output
 console = Console()
 
+# Video Feed
+camera = cv2.VideoCapture(0)
+
+# Custom YOLOv11 Model 
+model = YOLO("model/best.pt")
+
 def main():
-    model = YOLO("model/best.pt")
-    result = model.predict("tests/data/01.jpg", save=True)
-    result.show()
+    try:
+        while True:
+            status, frame = camera.read()
+            results = model(frame)
+            cv2.imshow("CaptureSmiles", results[0].plot())
+            print(results)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+    except KeyboardInterrupt:
+        console.print("[bold red]Exiting...[/bold red]")
         
+    except Exception as e:
+        console.print(f"[bold red]Error: {e}[/bold red]")
         
+    finally:
+        camera.release()
+        cv2.destroyAllWindows()
+        console.print("✅ [bold green]Done[/bold green]")
+
 if __name__ == "__main__":
     main()
