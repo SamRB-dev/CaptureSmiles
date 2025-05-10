c4e9ddc1b662d357f386e47c39754e629ce96351
SamRB-dev <sadimrahmanbadhon@gmail.com>
2025-05-10 14:35:09 +0200

    Detect smile , confidence 0.6, same the frame on the directory

--- CaptureSmiles.py
+++ CaptureSmiles.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from ultralytics import YOLO
 from rich.console import Console
-import cv2 
+import cv2, datetime
 
 # Rich Console for colored output
 console = Console()
@@ -10,15 +10,17 @@ console = Console()
 camera = cv2.VideoCapture(0)
 
 # Custom YOLOv11 Model 
-model = YOLO("model/best.pt")
+model = YOLO("model/Smile_Detector_v2.pt")
 
-def main():
+def main() -> None:
     try:
         while True:
             status, frame = camera.read()
             results = model(frame)
             cv2.imshow("CaptureSmiles", results[0].plot())
-            print(results)
+            if len(results[0].boxes.conf) != 0 and results[0].boxes.conf[0] > 0.6:
+                console.print(f"✅ [bold green]Smile detected![/bold green]")
+                cv2.imwrite(f"smile_{datetime.datetime.now().strftime('%Y-%m-%d-%H:%M:%S')}.jpg", frame)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
     except KeyboardInterrupt:
